884cb21c609a859fc233c86e5bfe1ac563ff554a
Pratham Chauhan <Prathamchauhan2002@gmail.com>
2023-06-16 15:31:00 +0530

    fix

--- static/script/route.js
+++ static/script/route.js
@@ -1,10 +1,11 @@
 window.addEventListener('DOMContentLoaded', function() {
   // Get the current URL path
   const currentPath = window.location.pathname;
+  console.log(currentPath);
 
   // Define the content mapping based on routes
   const contentMapping = {
-    '/Attd-Frontend': 'home.html',
+    '/Attd-Frontend/': 'home.html',
   };
 
   // Get the corresponding content filename based on the current path
