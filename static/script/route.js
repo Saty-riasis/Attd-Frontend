3764d45a2808884700e63d9906f4ebdddc5e9003
Pratham Chauhan <Prathamchauhan2002@gmail.com>
2023-06-16 14:59:45 +0530

    js

---
+++ static/script/route.js
@@ -0,0 +1,23 @@
+window.addEventListener('DOMContentLoaded', function() {
+  // Get the current URL path
+  const currentPath = window.location.pathname;
+
+  // Define the content mapping based on routes
+  const contentMapping = {
+    '/': 'home.html',
+  };
+
+  // Get the corresponding content filename based on the current path
+  const contentFilename = contentMapping[currentPath];
+
+  if (contentFilename) {
+    // Fetch and inject the corresponding content HTML file
+    fetch(contentFilename)
+      .then(response => response.text())
+      .then(html => {
+        const contentElement = document.querySelector('#content');
+        contentElement.innerHTML = html;
+      })
+      .catch(error => console.error('Error:', error));
+  }
+});
